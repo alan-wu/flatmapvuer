6815f6bdb9d2bf1df146065bfa793b570f22d64b
alan-wu <alan.wu@auckland.ac.nz>
2020-03-11 16:18:43 +1300

    Add a multiflatmapvuer which provide species selection.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,20 @@
 <template>
   <div id="app">
-    <el-radio-group v-model="species" size="small">
-      <el-radio-button label="Human"></el-radio-button>
-      <el-radio-button label="Rat"></el-radio-button>
-    </el-radio-group>
-    <FlatmapVuer v-show="species=='Human'" entry="NCBITaxon:9606" ref="Human" v-on:resource-selected="FlatmapSelected" @ready="FlatmapReady" style="height:100%" key="Human"/>
-    <FlatmapVuer v-show="species=='Rat'" entry="NCBITaxon:10114" ref="Rat" v-on:resource-selected="FlatmapSelected" @ready="FlatmapReady" style="height:100%" key="Rat"/>
+    <MultiFlatmapVuer :availableSpecies="availableSpecies"/>
   </div>
 </template>
 
 <script>
 /* eslint-disable no-alert, no-console */
 import Vue from "vue";
-import FlatmapVuer from './components/FlatmapVuer.vue'
+import MultiFlatmapVuer from './components/MultiFlatmapVuer.vue'
 import {
   RadioButton,
   RadioGroup
 } from 'element-ui';
 import "./styles/purple/radio-button.css";
 import "./styles/purple/radio-group.css";
+import "./icons/species.css";
 Vue.use(RadioButton);
 Vue.use(RadioGroup);
 
@@ -34,26 +30,12 @@ export default {
   },
   data: function(){
     return {
-      taxo:"NCBITaxon:9606",
-      species:"Human"
+      availableSpecies : {"Human":{taxo: "NCBITaxon:9606", iconClass:"icon-noun_humans_193734"},
+        "Rat":{taxo: "NCBITaxon:10114", iconClass:"icon-noun_Rat_15130"} }
     }
   },
   components: {
-    FlatmapVuer
-  },
-  watch: {
-    species: function (val) {
-      switch (val) {
-        case "Human":
-          this.taxo = "NCBITaxon:9606";
-          break;
-        case "Rat":
-          this.taxo = "NCBITaxon:10114";
-          break;
-        default:
-          break;
-      }
-    }
+    MultiFlatmapVuer
   }
 }
 </script>
---
+++ src/components/MultiFlatmapVuer.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <div style="position:absolute;z-index:1;">
+      <el-select v-model="activeSpecies" placeholder="Select" class="select-box">
+        <el-option
+          v-for="(item, key) in availableSpecies"
+          :key="key"
+          :label="key"
+          :value="key">
+            <i :class="item.iconClass" style="padding-right:12px;"></i>{{ key }}
+        </el-option>
+      </el-select>
+    </div>
+    <FlatmapVuer v-for="(item, key) in availableSpecies" :key="key" :showLayer="showLayer" v-show="activeSpecies==key" :entry="item.taxo" :ref="key" v-on:resource-selected="FlatmapSelected" @ready="FlatmapReady" style="height:100%"/>
+  </div>
+</template>
+
+
+<script>
+/* eslint-disable no-alert, no-console */
+import Vue from "vue";
+import FlatmapVuer from './FlatmapVuer.vue'
+import {
+  Option,
+  Select
+} from "element-ui";
+import lang from "element-ui/lib/locale/lang/en";
+import locale from "element-ui/lib/locale";
+locale.use(lang);
+Vue.use(Option);
+Vue.use(Select);
+
+export default {
+  name: 'MultiFlatmapVuer',
+  components: {
+    FlatmapVuer
+  },
+  mounted: function() {
+    this.activeSpecies = Object.keys(this.availableSpecies)[0];
+      console.log(this.activeSpecies.length)
+  },
+  methods: {
+    FlatmapSelected: function(resource) {
+      console.log(resource)
+    },
+    FlatmapReady: function(component) {
+      console.log(component.getLabels());
+    }
+  },
+  props: {
+    showLayer: {
+      type: Boolean,
+      default: false,
+    },
+    availableSpecies: {}
+  },
+  data: function(){
+    return {
+      activeSpecies: undefined
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  height:100%;
+  width: 100%;
+}
+
+.select-box {
+  width: 124px;
+  border-radius: 4px;
+  border: solid 1px #8300bf;
+  background-color: var(--white);
+  font-weight:500;
+  color: #8300bf;
+  margin-left:19px;
+  margin-top: 54px;
+}
+
+.select-box >>> .el-input__inner {
+  color: #8300bf;
+}
+
+</style>
+
+<style scoped src="../styles/purple/select.css">
+</style>
+<style scoped src="../styles/purple/option.css">
+</style>
