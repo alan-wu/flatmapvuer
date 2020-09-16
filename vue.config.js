27fad8dd565dbfd53cf7fe04d2ea733b762d94c5
alan-wu <alan.wu@auckland.ac.nz>
2020-09-16 15:47:08 +1200

    Now built without bundling external node modules.

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,9 @@
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production') {
+      config.externals = [nodeExternals({}) ];
+    }
+  }
 }
