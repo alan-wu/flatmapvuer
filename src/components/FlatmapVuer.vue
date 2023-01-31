340997fcae965e8b64731d27effee631519fdd7e
alan-wu <alan.wu@auckland.ac.nz>
2023-01-31 15:33:10 +1300

    Support map initialisation with a preset searchTerm.

--- src/components/FlatmapVuer.vue
+++ src/components/FlatmapVuer.vue
@@ -703,7 +703,7 @@ export default {
         });
       } else if (state) {
         if (this.entry == state.entry) {
-          this._stateToBeSet = { viewport: state.viewport, searchTerm: searchTerm };
+          this._stateToBeSet = { viewport: state.viewport, searchTerm: state.searchTerm };
           if (this.mapImp && !this.loading)
             this.restoreMapState(this._stateToBeSet);
         }
