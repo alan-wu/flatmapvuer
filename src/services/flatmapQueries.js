bc68afe4a1a53e6ce2b84159c89fc37eaaa0f17c
alan-wu <alan.wu@auckland.ac.nz>
2023-04-28 10:41:27 +1200

    Rewrite flatmap queries using a basic object structure.

--- src/services/flatmapQueries.js
+++ src/services/flatmapQueries.js
@@ -1,36 +1,28 @@
 /* eslint-disable no-alert, no-console */
 // remove duplicates by stringifying the objects
-const removeDuplicates = function(arrayOfAnything){
-  return [...new Set(arrayOfAnything.map(e => JSON.stringify(e)))].map(e => JSON.parse(e)) 
+const removeDuplicates = function (arrayOfAnything) {
+  return [...new Set(arrayOfAnything.map(e => JSON.stringify(e)))].map(e => JSON.parse(e))
 }
 
-const inArray = function(ar1, ar2){
+const inArray = function (ar1, ar2) {
   let as1 = JSON.stringify(ar1)
   let as2 = JSON.stringify(ar2)
   return as1.indexOf(as2) !== -1
 }
 
-export class FlatmapQueries {
-  constructor(sparcApi, flatmapApi) {
-    this.sparcApi = sparcApi
-    this.flatmapApi = flatmapApi
-    this.destinations = []
-    this.origins = []
-    this.components = []
-    this.uberons = []
-    this.urls = []
-    this.controller = undefined
-    this.getOrganCuries().then(uberons=>{
-      this.uberons = uberons
-      this.createLabelLookup(uberons).then(lookUp=>{
-        this.lookUp = lookUp
-      })
-    })
-  }
+exports.FlatmapQueries = function(sparcApi, flatmapApi) {
+  this.sparcApi = sparcApi
+  this.flatmapApi = flatmapApi
+  this.destinations = []
+  this.origins = []
+  this.components = []
+  this.uberons = []
+  this.urls = []
+  this.controller = undefined
 
-  createTooltipData = function (eventData) {
+  this.createTooltipData = (eventData) => {
     let tooltipData = {
-      destinations: this.destinations, 
+      destinations: this.destinations,
       origins: this.origins,
       components: this.components,
       destinationsWithDatasets: this.destinationsWithDatasets,
@@ -38,43 +30,32 @@ export class FlatmapQueries {
       componentsWithDatasets: this.componentsWithDatasets,
       title: eventData.label,
       featureId: eventData.resource,
-      hyperlinks: eventData.feature.hyperlinks ? eventData.feature.hyperlinks : this.urls.map(url=>({url: url, id: "pubmed"})),
+      hyperlinks: eventData.feature.hyperlinks ? eventData.feature.hyperlinks : this.urls.map(url => ({ url: url, id: "pubmed" })),
     }
     return tooltipData
   }
 
-  getOrganCuries = function(){
-    return new Promise(resolve=> {
-    fetch(`${this.sparcAPI}get-organ-curies/`)
-      .then(response=>response.json())
-      .then(data=>{
-        resolve(data.uberon.array)
-      })
-    })
-  }
-
-  createComponentsLabelList = function(components, lookUp){
-    let labelList = []
-    components.forEach(n=>{
-      labelList.push(this.createLabelFromNeuralNode(n[0]), lookUp)
-      if (n.length === 2){
-        labelList.push(this.createLabelFromNeuralNode(n[1]), lookUp)
-      }
-    })
-    return labelList
-  }
-
-  createLabelLookup = function(uberons) {
-    return new Promise(resolve=> {
-      let uberonMap = {}
-      const data = { sql: this.buildLabelSqlStatement(uberons)}
-      fetch(`${this.flatmapApi}knowledge/query/`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
+  const getOrganCuries = () => {
+    return new Promise(resolve => {
+      fetch(`${this.sparcAPI}get-organ-curies/`)
+        .then(response => response.json())
+        .then(data => {
+          resolve(data.uberon.array)
         })
+    })
+  }
+
+  const createLabelLookup = (uberons) => {
+    return new Promise(resolve => {
+      let uberonMap = {}
+      const data = { sql: buildLabelSqlStatement(uberons) }
+      fetch(`${this.flatmapApi}knowledge/query/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
         .then(response => response.json())
         .then(payload => {
           const entity = payload.keys.indexOf("entity");
@@ -84,12 +65,20 @@ export class FlatmapQueries {
               uberonMap[pair[entity]] = pair[label];
             });
           }
-        resolve(uberonMap)
+          resolve(uberonMap)
         })
     })
   }
 
-  buildConnectivitySqlStatement = function (keastIds) {
+
+  getOrganCuries().then(uberons => {
+    this.uberons = uberons
+    createLabelLookup(uberons).then(lookUp => {
+      this.lookUp = lookUp
+    })
+  })
+
+  const buildConnectivitySqlStatement = (keastIds) =>{
     let sql = 'select knowledge from knowledge where entity in ('
     if (keastIds.length === 1) {
       sql += `'${keastIds[0]}')`
@@ -100,7 +89,8 @@ export class FlatmapQueries {
     }
     return sql
   }
-  buildLabelSqlStatement = function (uberons) {
+
+  const buildLabelSqlStatement = (uberons) => {
     let sql = 'select entity, label from labels where entity in ('
     if (uberons.length === 1) {
       sql += `'${uberons[0]}')`
@@ -112,7 +102,7 @@ export class FlatmapQueries {
     return sql
   }
 
-  findAllIdsFromConnectivity = function (connectivity) {
+  const findAllIdsFromConnectivity = (connectivity) => {
     let dnodes = connectivity.connectivity.flat() // get nodes from edgelist
     let nodes = [...new Set(dnodes)] // remove duplicates
     let found = []
@@ -126,7 +116,111 @@ export class FlatmapQueries {
     return [... new Set(found.flat())]
   }
 
-  flattenConntectivity = function (connectivity) {
+  this.retrieveFlatmapKnowledgeForEvent = async (eventData) =>{
+    // check if there is an existing query
+    if (this.controller) this.controller.abort();
+
+    // set up the abort controller
+    this.controller = new AbortController();
+    const signal = this.controller.signal;
+
+    const keastIds = eventData.resource
+    this.destinations = []
+    this.origins = []
+    this.components = []
+    if (!keastIds || keastIds.length == 0) return
+    const data = { sql: buildConnectivitySqlStatement(keastIds) };
+    let prom1 = new Promise(resolve => {
+      fetch(`${this.flatmapApi}knowledge/query/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        signal: signal
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (connectivityExists(data)) {
+            let connectivity = JSON.parse(data.values[0][0])
+            processConnectivity(connectivity).then(() => {
+              resolve(true)
+            })
+          } else {
+            console.log('No connectivity data found')
+            resolve(true)
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+          resolve(false)
+        })
+    })
+    let prom2 = pubmedQueryOnIds(eventData)
+    return await Promise.all([prom1, prom2])
+
+  }
+
+  const connectivityExists = (data) => {
+    if (data.values && data.values.length > 0 && JSON.parse(data.values[0][0]).connectivity && JSON.parse(data.values[0][0]).connectivity.length > 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const createLabelFromNeuralNode = (node, lookUp) => {
+    let label = lookUp[node[0]]
+    if (node.length === 2 && node[1].length > 0) {
+      node[1].forEach(n => {
+        if (lookUp[n] == undefined) {
+          label += `, ${n}`
+        } else {
+          label += `, ${lookUp[n]}`
+        }
+      })
+    }
+    return label
+  }
+
+  const flattenAndFindDatasets = (components, axons, dendrites) => {
+
+    // process the nodes for finding datasets (Note this is not critical to the tooltip, only for the 'search on components' button)
+    let componentsFlat = flattenConntectivity(components)
+    let axonsFlat = flattenConntectivity(axons)
+    let dendritesFlat = flattenConntectivity(dendrites)
+
+    // Filter for the anatomy which is annotated on datasets
+    this.destinationsWithDatasets = this.uberons.filter(ub => axonsFlat.indexOf(ub.id) !== -1)
+    this.originsWithDatasets = this.uberons.filter(ub => dendritesFlat.indexOf(ub.id) !== -1)
+    this.componentsWithDatasets = this.uberons.filter(ub => componentsFlat.indexOf(ub.id) !== -1)
+  }
+
+  const processConnectivity = (connectivity) => {
+    return new Promise(resolve => {
+
+      // Filter the origin and destinations from components
+      let components = findComponents(connectivity)
+
+      // Remove duplicates
+      let axons = removeDuplicates(connectivity.axons)
+      let dendrites = removeDuplicates(connectivity.dendrites)
+
+      // Create list of ids to get labels for
+      let conIds = findAllIdsFromConnectivity(connectivity)
+
+      // Create readable labels from the nodes. Setting this to 'this.origins' updates the display
+      createLabelLookup(conIds).then(lookUp => {
+        this.destinations = axons.map(a => createLabelFromNeuralNode(a, lookUp))
+        this.origins = dendrites.map(d => createLabelFromNeuralNode(d, lookUp))
+        this.components = components.map(c => createLabelFromNeuralNode(c, lookUp))
+        flattenAndFindDatasets(components, axons, dendrites)
+        resolve(true)
+      })
+    })
+  }
+
+  const flattenConntectivity = (connectivity) => {
     let dnodes = connectivity.flat() // get nodes from edgelist
     let nodes = [...new Set(dnodes)] // remove duplicates
     let found = []
@@ -140,7 +234,7 @@ export class FlatmapQueries {
     return found.flat()
   }
 
-  findComponents = function (connectivity) {
+  const findComponents = (connectivity) => {
     let dnodes = connectivity.connectivity.flat() // get nodes from edgelist
     let nodes = removeDuplicates(dnodes)
 
@@ -163,152 +257,11 @@ export class FlatmapQueries {
     return found
   }
 
-  retrieveFlatmapKnowledgeForEvent = async function(eventData){
-      // check if there is an existing query
-      if (this.controller) this.controller.abort();
-
-      // set up the abort controller
-      this.controller = new AbortController();
-      const signal = this.controller.signal;
-
-      const keastIds = eventData.resource
-      this.destinations = []
-      this.origins = []
-      this.components = []
-      if (!keastIds || keastIds.length == 0) return
-      const data = { sql: this.buildConnectivitySqlStatement(keastIds)};
-      let prom1 = new Promise(resolve=>{
-        fetch(`${this.flatmapApi}knowledge/query/`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-          signal: signal
-        })
-        .then(response => response.json())
-        .then(data => {
-          if(this.connectivityExists(data)){
-            let connectivity = JSON.parse(data.values[0][0])
-            this.processConnectivity(connectivity).then(()=>{
-              resolve(true)
-            })
-          } else {
-            console.log('No connectivity data found')
-            resolve(true)
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-          resolve(false)
-        })
-      })
-      let prom2 = this.pubmedQueryOnIds(eventData)
-      return await Promise.all([prom1, prom2])
-    
-  }
-
-  connectivityExists = function(data){
-    if (data.values && data.values.length > 0 && JSON.parse(data.values[0][0]).connectivity && JSON.parse(data.values[0][0]).connectivity.length > 0) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  createLabelFromNeuralNode = function(node, lookUp){
-    let label = lookUp[node[0]]
-    if (node.length === 2 && node[1].length > 0){
-      node[1].forEach(n=>{
-        if (lookUp[n] == undefined){
-          label += `, ${n}` 
-        } else {
-          label += `, ${lookUp[n]}`
-        }
-      })
-    }
-    return label
-  }
-
-  flattenAndFindDatasets(components, axons, dendrites){
-      
-    // process the nodes for finding datasets (Note this is not critical to the tooltip, only for the 'search on components' button)
-    let componentsFlat = this.flattenConntectivity(components)
-    let axonsFlat = this.flattenConntectivity(axons)
-    let dendritesFlat = this.flattenConntectivity(dendrites)
-
-    // Filter for the anatomy which is annotated on datasets
-    this.destinationsWithDatasets = this.uberons.filter(ub => axonsFlat.indexOf(ub.id) !== -1)
-    this.originsWithDatasets = this.uberons.filter(ub => dendritesFlat.indexOf(ub.id) !== -1)
-    this.componentsWithDatasets = this.uberons.filter(ub => componentsFlat.indexOf(ub.id) !== -1)
-  }
-
-  processConnectivity(connectivity){
-    return new Promise (resolve=>{
-
-      // Filter the origin and destinations from components
-      let components = this.findComponents(connectivity)
-
-      // Remove duplicates
-      let axons = removeDuplicates(connectivity.axons)
-      let dendrites = removeDuplicates(connectivity.dendrites)
-
-      // Create list of ids to get labels for
-      let conIds = this.findAllIdsFromConnectivity(connectivity)  
-
-      // Create readable labels from the nodes. Setting this to 'this.origins' updates the display
-      this.createLabelLookup(conIds).then(lookUp=>{
-        this.destinations = axons.map(a=>this.createLabelFromNeuralNode(a,lookUp))
-        this.origins = dendrites.map(d=>this.createLabelFromNeuralNode(d,lookUp))
-        this.components = components.map(c=>this.createLabelFromNeuralNode(c, lookUp))
-        this.flattenAndFindDatasets(components, axons, dendrites)
-        resolve(true)
-      })
-    })
-  }
-
-  flattenConntectivity(connectivity){
-    let dnodes = connectivity.flat() // get nodes from edgelist
-    let nodes = [...new Set(dnodes)] // remove duplicates
-    let found = []
-    nodes.forEach(n=>{
-      if (Array.isArray(n)){
-        found.push(n.flat())
-      } else {
-        found.push(n)
-      }
-    })
-    return found.flat()
-  }
-
-  findComponents = function(connectivity){
-    let dnodes = connectivity.connectivity.flat() // get nodes from edgelist
-    let nodes = removeDuplicates(dnodes)
-
-    let found = []
-    let terminal = false
-    nodes.forEach(node=>{
-      terminal = false
-      // Check if the node is an destination or origin (note that they are labelled dendrite and axon as opposed to origin and destination)
-      if(inArray(connectivity.axons,node)){
-        terminal = true
-      }
-      if(inArray(connectivity.dendrites, node)){
-        terminal = true
-      }
-      if (!terminal){
-        found.push(node)
-      }
-    })
-
-    return found
-  }
-
-  stripPMIDPrefix = function (pubmedId){
+  const stripPMIDPrefix = (pubmedId) => {
     return pubmedId.split(':')[1]
   }
 
-  buildPubmedSqlStatement = function(keastIds) {
+  const buildPubmedSqlStatement = (keastIds) => {
     let sql = 'select distinct publication from publications where entity in ('
     if (keastIds.length === 1) {
       sql += `'${keastIds[0]}')`
@@ -320,12 +273,12 @@ export class FlatmapQueries {
     return sql
   }
 
-  buildPubmedSqlStatementForModels = function(model) {
+  const buildPubmedSqlStatementForModels = (model) => {
     return `select distinct publication from publications where entity = '${model}'`
   }
 
-  flatmapQuery = function(sql){
-    const data = { sql: sql}
+  const flatmapQuery = (sql) => {
+    const data = { sql: sql }
     return fetch(`${this.flatmapApi}knowledge/query/`, {
       method: 'POST',
       headers: {
@@ -333,49 +286,43 @@ export class FlatmapQueries {
       },
       body: JSON.stringify(data),
     })
-    .then(response => response.json())
-    .catch((error) => {
-      console.error('Error:', error)
-    })
+      .then(response => response.json())
+      .catch((error) => {
+        console.error('Error:', error)
+      })
   }
 
-  pubmedQueryOnIds = function(eventData){
+  const pubmedQueryOnIds = (eventData) => {
     const keastIds = eventData.resource
     const source = eventData.feature.source
-    if(!keastIds || keastIds.length === 0) return
-    const sql = this.buildPubmedSqlStatement(keastIds)
-    return this.flatmapQuery(sql).then(data=>{
+    if (!keastIds || keastIds.length === 0) return
+    const sql = buildPubmedSqlStatement(keastIds)
+    return flatmapQuery(sql).then(data => {
       // Create pubmed url on paths if we have them
-      if (data.values.length > 0){
-        this.urls = [this.pubmedSearchUrl(data.values.map(id=>this.stripPMIDPrefix(id[0])))]
+      if (data.values.length > 0) {
+        this.urls = [pubmedSearchUrl(data.values.map(id => stripPMIDPrefix(id[0])))]
         return true
       } else { // Create pubmed url on models
-        this.pubmedQueryOnModels(source).then(()=>{return true})
+        pubmedQueryOnModels(source).then(() => { return true })
       }
     })
   }
 
-  pubmedQueryOnModels = function(source){
-    return this.flatmapQuery(this.buildPubmedSqlStatementForModels(source)).then(data=>{
-      if (Array.isArray(data.values) && data.values.length > 0){
-        this.urls = [this.pubmedSearchUrl(data.values.map(id=>this.stripPMIDPrefix(id[0])))]
+  const pubmedQueryOnModels = (source) => {
+    return flatmapQuery(buildPubmedSqlStatementForModels(source)).then(data => {
+      if (Array.isArray(data.values) && data.values.length > 0) {
+        this.urls = [pubmedSearchUrl(data.values.map(id => stripPMIDPrefix(id[0])))]
       } else {
         this.urls = [] // Clears the pubmed search button 
-      } 
+      }
       return
     })
   }
 
-  pubmedSearchUrl = function(ids) {
+  const pubmedSearchUrl = (ids) => {
     let url = 'https://pubmed.ncbi.nlm.nih.gov/?'
     let params = new URLSearchParams()
     params.append('term', ids)
     return url + params.toString()
   }
 }
-
-
-
-
-
-export default FlatmapQueries
