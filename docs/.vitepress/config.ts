53f24a1c0e734a979e0689fad99f548e9dbd6468
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-02-29 16:56:17 +1300

    Add demo page in docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vitepress'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -8,23 +10,54 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
-      { text: 'API References', link: '/components/FlatmapVuer' },
+      { text: 'FlatmapVuer', link: '/components/FlatmapVuer' },
+      { text: 'MultiFlatmapVuer', link: '/components/MultiFlatmapVuer' },
     ],
 
     sidebar: [
       {
         text: 'Examples',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-          { text: 'API References', link: '/components/FlatmapVuer' },
+          { text: 'Live demo', link: '/demo' }
+        ]
+      },
+      {
+        text: 'API References',
+        items: [
+          { text: 'FlatmapVuer', link: '/components/FlatmapVuer' },
+          { text: 'MultiFlatmapVuer', link: '/components/MultiFlatmapVuer' },
         ]
       }
     ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/ABI-Software/flatmapvuer' }
+    ]
+  },
+  markdown: { attrs: { disable: true } },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use '../src/assets/styles' as *;`
+        },
+      },
+    },
+    plugins: [
+      Components({
+        // allow auto load markdown components under `./src/components/`
+        extensions: ['vue'],
+        // allow auto import and register components used in markdown
+        include: [/\.vue$/, /\.vue\?vue/],
+        resolvers: [
+          ElementPlusResolver({
+            importStyle: 'css',
+          }),
+        ],
+      }),
+
+      // https://github.com/antfu/unocss
+      // see unocss.config.ts for config
     ]
   }
 })
