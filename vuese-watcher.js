52f0fad65b5a46a20fee4645da571a466a5e7f7d
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-03-06 17:17:18 +1300

    Update docs vuese markdown render replace with parser and markdown-render

--- vuese-watcher.js
+++ vuese-watcher.js
@@ -5,26 +5,57 @@
  * so that Vitepress dev will refresh the docs for components
  */
 
+import fs from 'fs'
+import path from 'path'
 import chokidar from 'chokidar'
-import { exec } from 'child_process'
+import { parser } from '@vuese/parser'
+import { Render } from '@vuese/markdown-render'
 
 const componentsDir = 'src/components'
 const components = ['FlatmapVuer.vue', 'MultiFlatmapVuer.vue']
-
+const outputDir = 'docs/components'
 
 const watcher = chokidar.watch(components, {
   cwd: componentsDir,
   ignoreInitial: true,
 })
 
-watcher.on('change', (path) => {
-  console.log(`The component ${path} has changed!`)
+function generateMarkdown(file) {
+  const fileWithPath = `${componentsDir}/${file}`
+  const fileContent = fs.readFileSync(fileWithPath, 'utf-8')
 
-  exec('npm run vuese-gen', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing vuese command: ${error}`)
-      return
+  try {
+    const parserResult = parser(fileContent)
+    const r = new Render(parserResult)
+    const renderResult = r.render()
+    const markdownResult = r.renderMarkdown()
+    const markdownContent = markdownResult.content
+    const componentName = path.basename(fileWithPath, '.vue')
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir)
     }
-    console.log(`Vuese generated ${stdout}`)
-  })
+
+    fs.writeFile(`${outputDir}/${componentName}.md`, markdownContent, (err) => {
+      if (err) {
+        console.error(`Error writing markdown file for ${componentName}`, err)
+      } else {
+        console.log(`Markdown file for ${componentName} is generated!`)
+      }
+    })
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+// Run on first load
+components.forEach((component) => {
+  console.log(`Write markdown file for ${component} on first load.`)
+  generateMarkdown(component)
+})
+
+// Run on file change
+watcher.on('change', (file) => {
+  console.log(`The component ${file} has changed!`)
+  generateMarkdown(file)
 })
