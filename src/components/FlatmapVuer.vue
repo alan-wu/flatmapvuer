c010f725d0d99034af0e5f76e2ec4569be71ebe8
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-05-10 11:52:00 +1200

    Fix help mode bug

--- src/components/FlatmapVuer.vue
+++ src/components/FlatmapVuer.vue
@@ -1825,7 +1825,7 @@ export default {
     },
     helpMode: function (newVal, oldVal) {
       if (newVal !== oldVal) {
-        this.setHelpMode(val)
+        this.setHelpMode(newVal)
       }
     },
     state: {
