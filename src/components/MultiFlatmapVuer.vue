be45cbce3cabde32f2af012eed82488ef2e764d1
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-05-13 17:13:02 +1200

    Update help-mode initial active item

--- src/components/MultiFlatmapVuer.vue
+++ src/components/MultiFlatmapVuer.vue
@@ -66,7 +66,7 @@
       @pathway-selection-changed="onSelectionsDataChanged"
       :minZoom="minZoom"
       :helpMode="activeSpecies == key && helpMode"
-      :helpModeActiveItem="helpModeActiveItem"
+      :helpModeActiveItem="(helpModeActiveItem - 1)"
       @help-mode-last-item="onHelpModeLastItem"
       :renderAtMounted="renderAtMounted"
       :displayMinimap="displayMinimap"
