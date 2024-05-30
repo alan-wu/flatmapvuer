aa8de189572cb34ba584f99cc895926b79ed7d22
alan-wu <alan.wu@auckland.ac.nz>
2024-05-30 16:46:26 +1200

    Add a fix to cypress testing.

--- cypress/component/MultiFlatmapVuer.cy.js
+++ cypress/component/MultiFlatmapVuer.cy.js
@@ -9,6 +9,14 @@ import { createPinia, setActivePinia } from 'pinia';
 
 describe('MultiFlatmapVuer', () => {
 
+  Cypress.on('uncaught:exception', (err) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    if (err.message.includes('Source "markers" already exists.'))
+      return false
+    return true
+  })
+
   beforeEach(() => {
     cy.viewport(1920, 1080);
     cy.fixture('MultiFlatmapProps.json').as('props');
@@ -106,7 +114,7 @@ describe('MultiFlatmapVuer', () => {
         }, [])
 
         // Check the pop up has the same information as when the test was created
-        cy.get('.subtitle').should('exist').contains('Observed in Rattus norvegicus species')
+        cy.get('.subtitle').should('exist').contains('Studied in Rattus norvegicus species')
         cy.get('[origin-item-label="Twelfth thoracic ganglion"]').should('exist')
         cy.get('[component-item-label="connective tissue, neck of urinary bladder"]').should('exist')
         cy.get('[destination-item-label="wall of blood vessel, Arteriole in connective tissue of bladder dome"]').should('exist')
