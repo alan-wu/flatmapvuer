be7b6f21f9d58c0e877f93335e11faf60e6e5ace
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-06-14 11:30:44 +1200

    Tidy up

--- src/App.vue
+++ src/App.vue
@@ -95,10 +95,9 @@
 </template>
 
 <script>
+/* eslint-disable no-alert, no-console */
 import { shallowRef } from 'vue';
 import { Setting as ElIconSetting } from '@element-plus/icons-vue'
-/* eslint-disable no-alert, no-console */
-import MultiFlatmapVuer from './components/MultiFlatmapVuer.vue'
 import {
   ElAutocomplete as Autocomplete,
   ElButton as Button,
@@ -107,6 +106,7 @@ import {
   ElRow as Row,
 } from 'element-plus'
 import './icons/mapicon-species-style.css'
+import MultiFlatmapVuer from './components/MultiFlatmapVuer.vue'
 import { HelpModeDialog } from '@abi-software/map-utilities'
 import '@abi-software/map-utilities/dist/style.css'
 
@@ -119,6 +119,7 @@ export default {
     ElIconSetting,
     Popover,
     Row,
+    MultiFlatmapVuer,
     HelpModeDialog,
   },
   methods: {
@@ -298,9 +299,6 @@ export default {
         this.helpModeActiveItem = 0;
       }
     }
-  },
-  components: {
-    MultiFlatmapVuer,
   },
 }
 </script>
