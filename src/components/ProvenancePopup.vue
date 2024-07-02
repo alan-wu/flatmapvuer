cda6f25a6d391e39114dc8dcb6f5af985f8f2583
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-07-03 10:47:19 +1200

    Remove utility components updates

--- src/components/ProvenancePopup.vue
+++ src/components/ProvenancePopup.vue
@@ -15,7 +15,7 @@
     <div class="block" v-else>
       <div class="title">{{ entry.featureId }}</div>
     </div>
-    <div v-if="featuresAlert" class="attribute-title-container">
+    <div v-if="featuresAlert">
       <span class="attribute-title">Alert</span>
       <el-popover
         width="250"
@@ -43,7 +43,7 @@
       <div v-show="showDetails" class="content-container scrollbar">
         {{ entry.paths }}
         <div v-if="entry.origins && entry.origins.length > 0" class="block">
-          <div class="attribute-title-container">
+          <div>
             <span class="attribute-title">Origin</span>
             <el-popover
               width="250"
@@ -84,9 +84,7 @@
           v-if="entry.components && entry.components.length > 0"
           class="block"
         >
-          <div class="attribute-title-container">
-            <div class="attribute-title">Components</div>
-          </div>
+          <div class="attribute-title">Components</div>
           <div
             v-for="(component, i) in entry.components"
             class="attribute-content"
@@ -104,7 +102,7 @@
           v-if="entry.destinations && entry.destinations.length > 0"
           class="block"
         >
-          <div class="attribute-title-container">
+          <div>
             <span class="attribute-title">Destination</span>
             <el-popover
               width="250"
@@ -308,18 +306,18 @@ export default {
   text-align: left;
   // width: 16em;
   line-height: 1.5em !important;
-  font-size: 18px;
+  font-size: 1em;
   font-family: Helvetica;
-  font-weight: bold;
+  font-weight: 500;
+  /* font-weight: bold; */
   padding-bottom: 8px;
-  color: $app-primary-color;
 }
 
 .block {
   margin-bottom: 0.5em;
 
   .main > &:first-of-type {
-    margin-right: 1em;
+    margin-right: 0.5em;
   }
 }
 
@@ -390,8 +388,12 @@ export default {
   min-width: 16rem;
 }
 
-.attribute-title-container {
-  margin-bottom: 0.5em;
+.title {
+  font-size: 18px;
+  font-weight: 500;
+  font-weight: bold;
+  padding-bottom: 8px;
+  color: rgb(131, 0, 191);
 }
 
 .attribute-title {
@@ -404,10 +406,6 @@ export default {
 .attribute-content {
   font-size: 14px;
   font-weight: 500;
-
-  &:last-of-type {
-    margin-bottom: 0.5em;
-  }
 }
 
 .popover-container {
