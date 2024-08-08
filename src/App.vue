0a47d6603177149f69b54aeca12339656eeb00eb
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-08-08 17:09:36 +1200

    Move flatmap api endpoint to env file

--- src/App.vue
+++ src/App.vue
@@ -288,10 +288,11 @@ export default {
       multiflatmapRef: null,
       mapSettings: [],
       sparcApi: import.meta.env.SPARC_API,
+      flatmapAPI: import.meta.env.FLATMAP_API,
       //flatmapAPI: "https://mapcore-demo.org/current/flatmap/v2/"
       //flatmapAPI: "https://mapcore-demo.org/devel/flatmap/v3/"
       //flatmapAPI: "https://mapcoe-demo.org/current/flatmap/v3/",
-      flatmapAPI: 'https://mapcore-demo.org/devel/flatmap/v4/',
+      // flatmapAPI: 'https://mapcore-demo.org/devel/flatmap/v4/',
       //flatmapAPI: "https://mapcore-demo.org/fccb/flatmap/"
       //flatmapAPI: "https://mapcore-demo.org/staging/flatmap/v1/"
       // flatmapAPI: "https://mapcore-demo.org/devel/flatmap/v1/",
