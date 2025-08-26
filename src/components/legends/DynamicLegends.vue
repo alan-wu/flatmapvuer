b10090b4bc901de5a78cb95ec3bb4768f9d473e4
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2025-08-26 12:28:45 +1200

    Add -webkit-clip-path

--- src/components/legends/DynamicLegends.vue
+++ src/components/legends/DynamicLegends.vue
@@ -182,6 +182,7 @@ export default {
   height: calc(20px * 0.866);
   background-color: transparent;
   clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+  -webkit-clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
 }
 
 .exoid {
