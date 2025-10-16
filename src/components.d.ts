a8ca0b6ac3525eca783a913ae12a55732ede3878
alan-wu <alan.wu@auckland.ac.nz>
2025-10-16 13:40:59 +1300

    Update flatmap-viewer 4.4.0

--- src/components.d.ts
+++ src/components.d.ts
@@ -28,6 +28,7 @@ declare module 'vue' {
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     FlatmapError: typeof import('./components/FlatmapError.vue')['default']
     FlatmapVuer: typeof import('./components/FlatmapVuer.vue')['default']
+    LegendItem: typeof import('./components/legends/LegendItem.vue')['default']
     MultiFlatmapVuer: typeof import('./components/MultiFlatmapVuer.vue')['default']
     SelectionsGroup: typeof import('./components/SelectionsGroup.vue')['default']
     SvgLegends: typeof import('./components/legends/SvgLegends.vue')['default']
