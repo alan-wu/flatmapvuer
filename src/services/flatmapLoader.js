bed0e2b0b4f93f8b6e32e0ebf7808adf42fc36e6
alan-wu <alan.wu@auckland.ac.nz>
2026-04-21 14:42:18 +1200

    Update flatmapviewer.

--- src/services/flatmapLoader.js
+++ src/services/flatmapLoader.js
@@ -1,6 +1,6 @@
 /**
  * a single source for the flatmap-viewer library import
  */
-import * as flatmap from 'https://cdn.jsdelivr.net/npm/@abi-software/flatmap-viewer@4.6.0/+esm';
+import * as flatmap from 'https://cdn.jsdelivr.net/npm/@abi-software/flatmap-viewer@4.6.1/+esm';
 
 export default flatmap;
